72e67bef27011ada1879fd4f9bfafde948f5b69e
Winton <wintondvllrs2@gmail.com>
2024-02-09 10:59:48 +0200

    litmus

--- build/server/pages/products.js
+++ build/server/pages/products.js
@@ -76,24 +76,22 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => (/* binding */ Products),
 /* harmony export */   "getServerSideProps": () => (/* binding */ getServerSideProps)
 /* harmony export */ });
-/* harmony import */ var _styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(670);
-/* harmony import */ var _styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_6___default = /*#__PURE__*/__webpack_require__.n(_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_6__);
+/* harmony import */ var _styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(670);
+/* harmony import */ var _styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_5__);
 /* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1664);
 /* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var _components_ProductPageHeader__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(3490);
 /* harmony import */ var react_markdown__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(3135);
-/* harmony import */ var next_image__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(5675);
-/* harmony import */ var next_image__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(next_image__WEBPACK_IMPORTED_MODULE_3__);
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(6689);
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_4__);
-/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(997);
-/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(6689);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__);
 var __webpack_async_dependencies__ = __webpack_handle_async_dependencies__([react_markdown__WEBPACK_IMPORTED_MODULE_2__]);
 react_markdown__WEBPACK_IMPORTED_MODULE_2__ = (__webpack_async_dependencies__.then ? (await __webpack_async_dependencies__)() : __webpack_async_dependencies__)[0];
 
 
 
-
+ // import Image from "next/image";
 
 
 
@@ -112,73 +110,73 @@ const getServerSideProps = async () => {
 function Products({
   sc_one_contents
 }) {
-  if (!sc_one_contents) return /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx("div", {
+  if (!sc_one_contents) return /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx("div", {
     children: "Loading ...."
   });
-  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.Fragment, {
-    children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx(_components_ProductPageHeader__WEBPACK_IMPORTED_MODULE_1__/* ["default"] */ .Z, {}), sc_one_contents.data.map(sc_one_content => /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsxs)("div", {
-      children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx("div", {
+  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.Fragment, {
+    children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx(_components_ProductPageHeader__WEBPACK_IMPORTED_MODULE_1__/* ["default"] */ .Z, {}), sc_one_contents.data.map(sc_one_content => /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsxs)("div", {
+      children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx("div", {
         id: "LOANS",
-        className: (_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_6___default().open_section),
-        children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx("h1", {
-          className: (_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_6___default().headline),
-          children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx("mark", {
-            className: (_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_6___default().mark),
+        className: (_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_5___default().open_section),
+        children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx("h1", {
+          className: (_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_5___default().headline),
+          children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx("mark", {
+            className: (_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_5___default().mark),
             children: "LOAN PRODUCTS"
           })
         })
-      }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx("div", {
-        className: (_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_6___default().card_content_area),
-        children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsxs)("div", {
-          className: (_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_6___default().cards),
-          children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsxs)("div", {
-            className: (_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_6___default().card),
-            children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx(react_markdown__WEBPACK_IMPORTED_MODULE_2__["default"], {
+      }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx("div", {
+        className: (_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_5___default().card_content_area),
+        children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsxs)("div", {
+          className: (_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_5___default().cards),
+          children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsxs)("div", {
+            className: (_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_5___default().card),
+            children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx(react_markdown__WEBPACK_IMPORTED_MODULE_2__["default"], {
               children: sc_one_content.attributes.PersonalLoans
-            }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx(react_markdown__WEBPACK_IMPORTED_MODULE_2__["default"], {
+            }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx(react_markdown__WEBPACK_IMPORTED_MODULE_2__["default"], {
               children: sc_one_content.attributes.TechCredit
             })]
-          }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx("div", {
-            className: (_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_6___default().card),
-            children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx(react_markdown__WEBPACK_IMPORTED_MODULE_2__["default"], {
+          }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx("div", {
+            className: (_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_5___default().card),
+            children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx(react_markdown__WEBPACK_IMPORTED_MODULE_2__["default"], {
               children: sc_one_content.attributes.ConsolidationLoans
             })
-          }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsxs)("div", {
-            className: (_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_6___default().card),
-            children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx(react_markdown__WEBPACK_IMPORTED_MODULE_2__["default"], {
+          }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsxs)("div", {
+            className: (_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_5___default().card),
+            children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx(react_markdown__WEBPACK_IMPORTED_MODULE_2__["default"], {
               children: sc_one_content.attributes.TwelvePercentLoans
-            }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx(react_markdown__WEBPACK_IMPORTED_MODULE_2__["default"], {
+            }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx(react_markdown__WEBPACK_IMPORTED_MODULE_2__["default"], {
               children: sc_one_content.attributes.NCRLoans
             })]
           })]
         })
-      }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx("div", {
+      }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx("div", {
         id: "CREDITCARD",
-        className: (_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_6___default().open_section),
-        children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx("h1", {
-          className: (_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_6___default().headline),
-          children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx("mark", {
-            className: (_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_6___default().mark),
+        className: (_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_5___default().open_section),
+        children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx("h1", {
+          className: (_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_5___default().headline),
+          children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx("mark", {
+            className: (_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_5___default().mark),
             children: "CREDIT CARD"
           })
         })
-      }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx("div", {
-        className: (_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_6___default().card_content_area),
-        children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsxs)("div", {
-          className: (_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_6___default().cards),
-          children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx("div", {
-            className: (_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_6___default().card),
-            children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx(react_markdown__WEBPACK_IMPORTED_MODULE_2__["default"], {
+      }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx("div", {
+        className: (_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_5___default().card_content_area),
+        children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsxs)("div", {
+          className: (_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_5___default().cards),
+          children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx("div", {
+            className: (_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_5___default().card),
+            children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx(react_markdown__WEBPACK_IMPORTED_MODULE_2__["default"], {
               children: sc_one_content.attributes.CreditCard
             })
-          }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx("div", {
-            className: (_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_6___default().card),
-            children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx(react_markdown__WEBPACK_IMPORTED_MODULE_2__["default"], {
+          }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx("div", {
+            className: (_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_5___default().card),
+            children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx(react_markdown__WEBPACK_IMPORTED_MODULE_2__["default"], {
               children: sc_one_content.attributes.CreditCardFees
             })
-          }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx("div", {
-            className: (_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_6___default().card),
-            children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_5__.jsx(react_markdown__WEBPACK_IMPORTED_MODULE_2__["default"], {
+          }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx("div", {
+            className: (_styles_Scripts_module_css__WEBPACK_IMPORTED_MODULE_5___default().card),
+            children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx(react_markdown__WEBPACK_IMPORTED_MODULE_2__["default"], {
               children: sc_one_content.attributes.CreditCardFeesMore
             })
           })]
@@ -189,20 +187,6 @@ function Products({
 }
 __webpack_async_result__();
 } catch(e) { __webpack_async_result__(e); } });
-
-/***/ }),
-
-/***/ 3918:
-/***/ ((module) => {
-
-module.exports = require("next/dist/shared/lib/amp-context.js");
-
-/***/ }),
-
-/***/ 5732:
-/***/ ((module) => {
-
-module.exports = require("next/dist/shared/lib/amp-mode.js");
 
 /***/ }),
 
@@ -224,34 +208,6 @@ module.exports = require("next/dist/shared/lib/head-manager-context.js");
 /***/ ((module) => {
 
 module.exports = require("next/dist/shared/lib/i18n/normalize-locale-path.js");
-
-/***/ }),
-
-/***/ 4486:
-/***/ ((module) => {
-
-module.exports = require("next/dist/shared/lib/image-blur-svg.js");
-
-/***/ }),
-
-/***/ 744:
-/***/ ((module) => {
-
-module.exports = require("next/dist/shared/lib/image-config-context.js");
-
-/***/ }),
-
-/***/ 5843:
-/***/ ((module) => {
-
-module.exports = require("next/dist/shared/lib/image-config.js");
-
-/***/ }),
-
-/***/ 9552:
-/***/ ((module) => {
-
-module.exports = require("next/dist/shared/lib/image-loader");
 
 /***/ }),
 
@@ -395,24 +351,10 @@ module.exports = require("next/dist/shared/lib/router/utils/route-regex.js");
 
 /***/ }),
 
-/***/ 2470:
-/***/ ((module) => {
-
-module.exports = require("next/dist/shared/lib/side-effect.js");
-
-/***/ }),
-
 /***/ 9232:
 /***/ ((module) => {
 
 module.exports = require("next/dist/shared/lib/utils.js");
-
-/***/ }),
-
-/***/ 618:
-/***/ ((module) => {
-
-module.exports = require("next/dist/shared/lib/utils/warn-once.js");
 
 /***/ }),
 
@@ -451,7 +393,7 @@ module.exports = import("react-markdown");;
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = __webpack_require__.X(0, [210,676,664,505,675,670], () => (__webpack_exec__(8910)));
+var __webpack_exports__ = __webpack_require__.X(0, [210,676,664,670], () => (__webpack_exec__(8910)));
 module.exports = __webpack_exports__;
 
 })();
